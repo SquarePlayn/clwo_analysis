8fd245aa9047cf1a057483fe5c684eb68eed636f
Square Play'n <pietervoors@hotmail.com>
2020-06-03 01:18:48 +0200

    API loading function

--- utility.py
+++ utility.py
@@ -1,3 +1,9 @@
-def load_api(api):
+import requests
+
+
+def fetch_api(api):
     """ Fetch the contents of an API """
-    pass
+    response = requests.get(api)
+    if response.status_code < 200 or 299 < response.status_code:
+        raise Exception("The following API is not available: " + api + ". Status code: " + response.status_code + ".")
+    return response.json()
