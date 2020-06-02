7192bc99c3eb142d1826a75dbd32354250625b06
Square Play'n <pietervoors@hotmail.com>
2020-06-03 01:39:32 +0200

    Clean code

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from database import Database
 from map_logging import *
 from settings import Settings
 
 
 def main():
-    print("Hello world!")
     # Initialize
+    print("Initializing")
     Settings.load_settings()
     Database.login()
 
     # Main work
+    print("Performing main tasks")
     log_map()
 
     # Shut down
+    print("Finalizing")
     Database.finalize()
 
 
